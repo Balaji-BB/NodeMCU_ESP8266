b59c921a051878a3d561649202e7d098df8d62b5
Balaji <balajib@tutanota.com>
2025-03-30 19:56:06 +0530

    Mail Alert using SMTP

---
+++ SMTP_Gmail_Alert/SMTP_Gmail_Alert.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+#include <ESP_Mail_Client.h>
+
+// WiFi Credentials
+#define WIFI_SSID "Airtel_Embedded_home"
+#define WIFI_PASSWORD "Embedded@1298"
+
+// SMTP Credentials
+#define SMTP_HOST "smtp.gmail.com"
+#define SMTP_PORT 465 // SSL port
+#define AUTHOR_EMAIL "bala7.bbi@gmail.com"
+#define AUTHOR_PASSWORD "dgwp dhel qedy zxzl"
+#define RECIPIENT_EMAIL "firedetectionnotification@gmail.com"
+
+// SMTP Session
+SMTPSession smtp;
+
+void setup() {
+    Serial.begin(115200);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected to WiFi!");
+
+    sendEmail();
+}
+
+void sendEmail() {
+    smtp.debug(1);
+    smtp.callback(smtpCallback);
+
+    ESP_Mail_Session session;
+    session.server.host_name = SMTP_HOST;
+    session.server.port = SMTP_PORT;
+    session.login.email = AUTHOR_EMAIL;
+    session.login.password = AUTHOR_PASSWORD;
+    session.login.user_domain = "";
+
+    SMTP_Message message;
+    message.sender.name = "ESP8266 Alert";
+    message.sender.email = AUTHOR_EMAIL;
+    message.subject = "ESP8266 Alert: Motion Detected";
+    message.addRecipient("User", RECIPIENT_EMAIL);
+    message.text.content = "This is an automated alert from your ESP8266 system.";
+    message.text.charSet = "utf-8";
+    message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+    if (!smtp.connect(&session)) {
+        Serial.println("SMTP Connection Failed!");
+        return;
+    }
+
+    if (MailClient.sendMail(&smtp, &message)) {
+        Serial.println("Email Sent Successfully!");
+    } else {
+        Serial.println("Error Sending Email: " + smtp.errorReason());
+    }
+}
+
+void smtpCallback(SMTP_Status status) {
+    Serial.println(status.info());
+}
+
+void loop() {
+    // Add event triggers here (e.g., motion detection)
+}
